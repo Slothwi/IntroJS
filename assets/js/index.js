3add22802c80111303742c47e201723c827cc493
Gabriel Perez <gabo.leonp@gmail.com>
2023-01-05 22:30:43 -0300

    Proyecto Final

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,13 @@
 function calcular () {
-    const cantidad = document
-}
+    const cantidad = document .querySelector('#cantidad').value;
+    const color = document .querySelector('#color').value;
+    const precio = document .querySelector('#precio').innerHTML;
+
+    console.log('Cantidad: ', cantidad)
+    console.log('Color: ', color)
+    console.log('precio: ', precio)
+
+    document.querySelector('#resumen_total').innerHTML = cantidad * precio
+    document.querySelector('#resumen_cantidad').innerHTML = cantidad
+    document.querySelector('#resumen_color').style.backgroundColor = color 
+}   
